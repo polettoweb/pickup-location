088f66327e812c268ba4ce6f7cb019f6913d60a0
Marco Poletto <marco@marcopoletto.eu>
2019-01-22 21:39:04 +0000

    Final logic and touches to CSS

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,122 @@
 <template>
-  <div id="app">
-    <form id="form" name="pickupLocation">
-      <h2>Let’s find your ideal car</h2>
-      <fieldset id="location-fieldset">
-          <div class="location-input">
-            <label for="ftsAutocomplete">Pick-up Location</label>
-            <input type="text" name="location" autocomplete="off" :value="location" @input="retrieveLocations" placeholder="city, airport, station, region, district…" aria-required="true" class="ui-autocomplete-input" role="textbox" aria-autocomplete="list" aria-haspopup="true">
-          </div>
-          <div class="location-list">
-              <ul>
-                  <li v-for="specificLocation in listOfResults" :key="specificLocation.bookingId" role="menuitem">
-                      <a>
-                          <span :class="`location-type-${specificLocation.placeType ? specificLocation.placeType.toLowerCase() : ''}`"></span>
-                          <div class="location-name">
-                              <p v-html="highlight(specificLocation.name)"></p>
-                              <span>{{specificLocation.region}}, {{specificLocation.country}}</span>
-                          </div>
-                    </a>
-                    </li>
-              </ul>
-          </div>
-      </fieldset>
-    </form>
-  </div>
+    <div id="app">
+        <form id="form" name="pickupLocation">
+            <h2>Let’s find your ideal car</h2>
+            <fieldset id="location-fieldset">
+                <div class="location-input">
+                    <label for="location">Pick-up Location</label>
+                    <input
+                        type="text"
+                        name="location"
+                        autocomplete="off"
+                        :value="location"
+                        @input="retrieveLocations"
+                        @focus="searchActive = true"
+                        placeholder="city, airport, station, region, district…"
+                        class="ui-autocomplete-input"
+                        role="textbox"
+                        aria-required="true"
+                        aria-autocomplete="list"
+                        aria-haspopup="true"
+                    >
+                </div>
+                <div class="location-list" v-show="searchActive">
+                    <ul>
+                        <li
+                        v-for="specificLocation in listOfResults"
+                        :key="specificLocation.bookingId"
+                        role="menuitem"
+                        tabindex="0"
+                        :class="{'location-no-results': numberInList > 0 &&  specificLocation.bookingId === undefined}"
+                        @keyup.enter="selectLocation(specificLocation.name,hasIata(specificLocation.iata), specificLocation.region, specificLocation.country)"
+                        >
+                            <a v-if="numberInList > 0 &&  specificLocation.bookingId === undefined" @click="searchActive = false">
+                                <div class="location-name">
+                                    <p>{{specificLocation.name}}</p>
+                                </div>
+                            </a>
+                            <a v-else @click="selectLocation(specificLocation.name,hasIata(specificLocation.iata), specificLocation.region, specificLocation.country)">
+                                <span :class="`location-type location-type${specificLocation.placeType ? `-${specificLocation.placeType.toLowerCase()}` : ''}`"></span>
+                                <div class="location-name">
+                                    <p v-html="highlight(specificLocation.name,hasIata(specificLocation.iata))"></p>
+                                    <span class="location-region-country">
+                                        {{specificLocation.region}}, {{specificLocation.country}}
+                                    </span>
+                                    <!-- couldn't find any result with isPopular = true.
+                                    for testing it please change to v-if="!specificLocation.isPopular"-->
+                                    <span class="location-popular" v-if="specificLocation.isPopular">Popular</span>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </fieldset>
+        </form>
+    </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  data() {
-    return {
-      location: '',
-      numberOfResults: 6,
-      listOfResults: null,
-      boldName: null,
-      normalName: null
+    data() {
+        return {
+            location: "", //used as model for input value
+            numberOfResults: 6, //number of max results for the API call
+            listOfResults: null, //storing the result of the API
+            debounceTimer: null, //used for debouncing the API call
+            numberInList: 0, //used for checking if the API returns more than 1 result
+            searchActive: false //opening and closing the result panel
+        };
+    },
+    methods: {
+        //method associated with @input
+        retrieveLocations(event) {
+            this.location = event.target.value;
+            const url = `https://cors.io/?https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=${this.numberOfResults}&solrTerm=${this.location}`;
+
+            //calling the API only in case the search is greater than 1 char
+            if (this.location.length > 1) {
+                this.searchActive = true;
+
+                //debouncing the search for avoiding eccessive numbers of calls to the API
+                clearTimeout(this.debounceTimer);
+                this.debounceTimer = setTimeout(() => {
+                    axios.get(url).then(res => {
+                        this.listOfResults = res.data.results.docs;
+
+                        this.numberInList = res.data.results.docs.length;
+                    });
+                }, 300);
+            } else {
+                //resetting the search and close the result panel
+                this.listOfResults = null;
+                this.searchActive = false;
+            }
+        },
+        //method used for matching user search and result with a bold text and appending the IATA if applicable
+        highlight(string, append) {
+            if (!this.location) return;
+            //string replace location with result checking all matches with insensitive case
+            const locationFirstPart = string.replace(
+                new RegExp(this.location, "gi"),
+                match => "<em>" + match + "</em>"
+            );
+            return locationFirstPart + " " + append;
+        },
+        //checking if result has IATA or not
+        hasIata(string) {
+            return string !== undefined ? `(${string})` : "";
+        },
+        //on location selected, filling the input with correct data
+        selectLocation(name, iata, region, country) {
+            this.location = iata === "" ? `${name}, ${region}, ${country}` : `${name} ${iata}, ${region}, ${country}`;
+            this.searchActive = false; //closing the result panel
+        }
     }
-  },
-  methods: {
-    retrieveLocations(event) {
-      this.location = event.target.value;
-      if (this.location.length > 1) {
-        let url = `https://cors.io/?https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=${this.numberOfResults}&solrTerm=${this.location}`;
-        axios.get(url)
-        .then(res => {
-          this.listOfResults = res.data.results.docs;
-        })
-      } else {
-          this.listOfResults = '';
-      }
-      
-    },
-    highlight(string) {
-        if(!this.location) return;
-        return string.replace(new RegExp(this.location, "gi"), match => {
-            return '<em>' + match + '</em>' ;
-        });
-    },
-    nameSplit(value) {
-          if (!value) return ''
-          return value.replace(this.location.charAt(0).toUpperCase() + this.location.substr(1), "").trim();
-      }
-  }
-}
+};
 </script>
 
 <style lang="scss">
-* {
-  padding: 0;
-  margin: 0;
-}
-html, body {
-  font-family: Ubuntu, Helvetica, Arial, sans-serif;
-}
-#form {
-    margin: 26px auto;
-    padding: 26px;
-    width: 50%;
-    background: #F3CE56;
-    background: linear-gradient(to bottom,  #f5d361 0%,#e7bf3b 100%);
-    border-bottom: 2px solid #DAB129;
-
-    fieldset {
-      border: none;
-    }
-    .location-input {
-          line-height: 30px;
-          padding: 0 0 5px;
-
-          label {
-                color: #444;
-                text-shadow: none;
-                font-size: 0.875;
-          }
-
-          input {
-                padding: 12px 8% 12px 2%;
-                width: 90%;
-                margin: 0;
-                background: #FEFEFE;
-                border-color: #a9a9a9;
-                border-style: solid;
-                border-width: 1px;
-                font-size: 0.813em;
-          }
-    }
-}
-em {
-    font-weight: 700;
-    font-style: normal;
-}
+    @import "./assets/scss/main.scss";
 </style>
